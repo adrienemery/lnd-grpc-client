d710ac24b56c9e55e618f208841d12cb3c5452de
Adrien Emery <adrien.emery@gmail.com>
2018-05-03 16:21:09 -0700

    Bumpversion 0.1.2 -> 0.1.3

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     packages=find_packages(exclude=exclude_packages),
     install_requires=install_requires,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
-    version='0.1.2',
+    version='0.1.3',
     description='An rpc client for LND (lightning network deamon)',
     long_description=long_description,
     author='Adrien Emery',
