c1924d45a71a0b1e61018a8e489c406b7a5c42e8
Tim Akinbo <tim@akinbo.org>
2019-03-19 18:06:31 +0100

    rename module from async to aio to prevent reserved word collision

--- lndgrpc/__init__.py
+++ lndgrpc/__init__.py
@@ -7,4 +7,4 @@ MINOR = sys.version_info[1]
 
 # only import the async client for python 3.6+
 if MAJOR == 3 and MINOR >= 6:
-    from lndgrpc.async.async_client import AsyncLNDClient
+    from lndgrpc.aio.async_client import AsyncLNDClient
