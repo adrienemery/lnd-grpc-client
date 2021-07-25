475e46eba745f69f892d83ae83ddd8176779d2e3
Sam Korn <korn94sam@gmail.com>
2021-07-24 21:52:13 -0600

    update example to work better on remote or on-node

--- examples/example.py
+++ examples/example.py
@@ -10,14 +10,23 @@ from datetime import datetime, timedelta
 from lndgrpc import LNDClient
 from protobuf_to_dict import protobuf_to_dict
 
-credential_path = Path("/home/skorn/.lnd")
+credential_path = os.getenv("LND_CRED_PATH", None)
+if credential_path == None:
+	credential_path = Path("/home/skorn/.lnd/")
+	mac = str(credential_path.joinpath("data/chain/bitcoin/mainnet/admin.macaroon").absolute())
+else:
+	credential_path = Path(credential_path)
+	mac = str(credential_path.joinpath("admin.macaroon").absolute())
+	
 
-mac = str(credential_path.joinpath("data/chain/bitcoin/mainnet/admin.macaroon").absolute())
+node_ip = "192.168.1.58"
 tls = str(credential_path.joinpath("tls.cert").absolute())
 
 
 lnd = LNDClient(
-	"127.0.0.1:10009",
+	f"{node_ip}:10009",
 	macaroon_filepath=mac,
 	cert_filepath=tls
 )
+
+lnd.get_info()
