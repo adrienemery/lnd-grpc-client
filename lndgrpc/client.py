13f067e54ce9a032703fecf642790a08e58de973
Sam Korn <korn94sam@gmail.com>
2022-06-18 20:01:54 -0600

    add cli entrypoint

--- lndgrpc/client.py
+++ lndgrpc/client.py
@@ -1,7 +1,8 @@
-from .common import walletunlocker, ver, walletkit, signer, router, ln, BaseClient, invoices
-from .errors import handle_rpc_errors
 from datetime import datetime
 import binascii
+
+from .common import walletunlocker, ver, walletkit, signer, router, ln, BaseClient, invoices
+from .errors import handle_rpc_errors
 
 from lndgrpc.router import RouterRPC
 from lndgrpc.walletunlocker import WalletUnlockerRPC
@@ -37,3 +38,31 @@ class LNDClient(
     StateRPC
 ):
     pass
+
+
+def cli():
+    import os
+    import code
+    from pathlib import Path
+    credential_path = os.getenv("LND_CRED_PATH", None)
+    if credential_path == None:
+        credential_path = Path.home().joinpath(".lnd")
+        mac = str(credential_path.joinpath("data/chain/bitcoin/mainnet/admin.macaroon").absolute())
+    else:
+        credential_path = Path(credential_path)
+        mac = str(credential_path.joinpath("admin.macaroon").absolute())
+
+    node_ip = os.getenv("LND_NODE_IP")
+    node_port = os.getenv("LND_NODE_PORT")
+    tls = str(credential_path.joinpath("tls.cert").absolute())
+
+    lnd_ip_port = f"{node_ip}:{node_port}"
+
+    lnd = LNDClient(
+        lnd_ip_port,
+        macaroon_filepath=mac,
+        cert_filepath=tls
+        # no_tls=True
+    )
+
+    code.interact(local=dict(globals(), **locals()))  
